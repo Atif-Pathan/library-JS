2b6d4a162127d78a1c8e3442bc3f2efbdc5adc9a
Atif Pathan <atifkhan.p1@gmail.com>
2024-10-29 20:00:20 -0600

    setup form for user input to add books and add basic styling to everything

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-// using array to store all the books in the library
-const myLibrary = [];
+document.addEventListener('DOMContentLoaded', function() {
+  // using array to store all the books in the library
+  const myLibrary = [];
+  const addBookBtn = document.querySelector(".add-book-btn");
 
-function Book(title, author, pages, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
-    this.info = function() {
-        return title + ", by " + author + " is " + pages + " pages - " + (hasRead ? 'Completed' : 'Not read yet');
-    }
-}
+  function Book(title, author, pages, hasRead) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.hasRead = hasRead;
+      this.info = function() {
+          return title + ", by " + author + " is " + pages + " pages - " + (hasRead ? 'Completed' : 'Not read yet');
+      }
+  }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+  function addBookToLibrary() {
+    let title = prompt("Enter book title:");
+    let author = prompt("Enter author:");
+    let pages = prompt("how many pages does it have?");
+    let hasRead = prompt("have you read it?");
+    console.log(title + ", " + author + ", " + pages + ", " + hasRead);
 
+    
+  }
 
-const theHobbit  = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+  addBookBtn.addEventListener("click", addBookToLibrary);
+
+  // const theHobbit  = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+  // myLibrary.push(theHobbit);
+  // console.log(theHobbit.info());
+});
