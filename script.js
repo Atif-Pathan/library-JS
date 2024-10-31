962b81f6e3ac72f673e299a3fc34465a364fa081
Atif Pathan <atifkhan.p1@gmail.com>
2024-10-30 19:29:53 -0600

    add toggling of form based on "add" and "cancel" buttons

--- script.js
+++ script.js
@@ -2,31 +2,49 @@ document.addEventListener('DOMContentLoaded', function() {
   // using array to store all the books in the library
   const myLibrary = [];
   const addBookBtn = document.querySelector(".add-book-btn");
+  const bookStatCheckBox = document.getElementById("book-status");
+  const ratingsDiv = document.querySelector(".rating-group");
+  const addBookForm = document.querySelector(".add-book-form");
+  const cancelBtn = document.querySelector(".cancel-book-btn");
 
   function Book(title, author, pages, hasRead) {
       this.title = title;
       this.author = author;
       this.pages = pages;
       this.hasRead = hasRead;
+      this.rating = 0;
       this.info = function() {
           return title + ", by " + author + " is " + pages + " pages - " + (hasRead ? 'Completed' : 'Not read yet');
       }
   }
 
   function addBookToLibrary() {
-    let title = prompt("Enter book title:");
-    let author = prompt("Enter author:");
-    let pages = prompt("how many pages does it have?");
-    let hasRead = prompt("have you read it?");
-    console.log(title + ", " + author + ", " + pages + ", " + hasRead);
-
-    
+    addBookForm.style.display = "grid";
   }
 
   addBookBtn.addEventListener("click", addBookToLibrary);
+  cancelBtn.addEventListener("click", function() {
+    addBookForm.style.display = "none";
+  });
+  bookStatCheckBox.addEventListener("change", function() {
+    if (this.checked) {
+      ratingsDiv.style.display = "inline-flex";
+    } else {
+      ratingsDiv.style.display = "None";
+    }
+  });
 
-  // const theHobbit  = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+  addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    new FormData(addBookForm);
 
-  // myLibrary.push(theHobbit);
-  // console.log(theHobbit.info());
+  })
+
+  addBookForm.addEventListener("formdata", (e) => {
+    const data = e.formData;
+    for (const value of data.values()) {
+      console.log(value);
+    }
+  })
+
 });
