e9e5f7447438e416a5e729e625afdee73a34f72c
Atif Pathan <atifkhan.p1@gmail.com>
2024-10-31 18:22:08 -0600

    submitting form creates a book object, which is stored in an array via the addBookToLibrary function
    Also, the form opens/closes and hides/unhides sections based on user input

--- script.js
+++ script.js
@@ -4,47 +4,66 @@ document.addEventListener('DOMContentLoaded', function() {
   const addBookBtn = document.querySelector(".add-book-btn");
   const bookStatCheckBox = document.getElementById("book-status");
   const ratingsDiv = document.querySelector(".rating-group");
-  const addBookForm = document.querySelector(".add-book-form");
+  const addBookForm = document.getElementById("add-book-form");
   const cancelBtn = document.querySelector(".cancel-book-btn");
+  const reviewDiv = document.querySelector(".book-review-div");
 
-  function Book(title, author, pages, hasRead) {
+  function Book(title, author, pages, review, hasRead, rating) {
       this.title = title;
       this.author = author;
       this.pages = pages;
+      this.review = review;
       this.hasRead = hasRead;
-      this.rating = 0;
+      this.rating = rating;
       this.info = function() {
           return title + ", by " + author + " is " + pages + " pages - " + (hasRead ? 'Completed' : 'Not read yet');
       }
   }
 
-  function addBookToLibrary() {
-    addBookForm.style.display = "grid";
+  function addBookToLibrary(data) {
+    if (data.has("book-status")) {
+      newBook = new Book(data.get("title"), data.get("author"), data.get("pages"), data.get("review"), true, data.get("rating2"));
+    }
+    else {
+      newBook = new Book(data.get("title"), data.get("author"), data.get("pages"), null, false, null);
+    }
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    cancelBtn.dispatchEvent(new Event("click"));
   }
 
-  addBookBtn.addEventListener("click", addBookToLibrary);
+  addBookBtn.addEventListener("click", function() {
+    addBookForm.style.display = "grid";
+  });
+
   cancelBtn.addEventListener("click", function() {
     addBookForm.style.display = "none";
+    addBookForm.reset();
+    bookStatCheckBox.dispatchEvent(new Event("change"));
   });
+
   bookStatCheckBox.addEventListener("change", function() {
     if (this.checked) {
       ratingsDiv.style.display = "inline-flex";
+      reviewDiv.style.display = "flex";
+      reviewDiv.style.gridColumn = "1/6";
+      reviewDiv.style.gridRow = "span 4";
+      reviewDiv.style.padding = "0 1rem";
+      reviewDiv.style.marginBottom = "0.3rem";
     } else {
-      ratingsDiv.style.display = "None";
+      ratingsDiv.style.display = "none";
+      reviewDiv.style.display = "none";
     }
   });
 
   addBookForm.addEventListener("submit", (e) => {
     e.preventDefault();
     new FormData(addBookForm);
-
   })
 
   addBookForm.addEventListener("formdata", (e) => {
     const data = e.formData;
-    for (const value of data.values()) {
-      console.log(value);
-    }
+    addBookToLibrary(data);
   })
 
 });
