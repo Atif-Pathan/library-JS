3e358d835fc780083bf0444d19cf1d1f9c97a4c8
Atif Pathan <atifkhan.p1@gmail.com>
2024-11-01 22:36:35 -0600

    add a synopsis and genre section and update javascript to create book object with those two additional datapoints

--- script.js
+++ script.js
@@ -9,24 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
   const reviewDiv = document.querySelector(".book-review-div");
   let bookCollection = document.querySelector(".book-collection");
 
-  function Book(title, author, pages, review, hasRead, rating) {
+  function Book(title, author, genre, synopsis, pages, review, hasRead, rating) {
       this.title = title;
       this.author = author;
+      this.genre = genre;
+      this.synopsis = synopsis;
       this.pages = pages;
       this.review = review;
       this.hasRead = hasRead;
       this.rating = rating;
-      this.info = function() {
-          return title + ", by " + author + " is " + pages + " pages - " + (hasRead ? 'Completed' : 'Not read yet');
-      }
   }
 
   function addBookToLibrary(data) {
     if (data.has("book-status")) {
-      newBook = new Book(data.get("title"), data.get("author"), data.get("pages"), data.get("review"), true, data.get("rating2"));
+      newBook = new Book(data.get("title"), data.get("author"), data.get("genre"), data.get("synopsis"), data.get("pages"), data.get("review"), true, data.get("rating2"));
     }
     else {
-      newBook = new Book(data.get("title"), data.get("author"), data.get("pages"), null, false, null);
+      newBook = new Book(data.get("title"), data.get("author"), data.get("genre"), data.get("synopsis"), data.get("pages"), null, false, null);
     }
     myLibrary.push(newBook);
     console.log(myLibrary);
@@ -55,8 +54,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (this.checked) {
       ratingsDiv.style.display = "inline-flex";
       reviewDiv.style.display = "flex";
-      reviewDiv.style.gridColumn = "1/6";
-      reviewDiv.style.gridRow = "span 4";
+      reviewDiv.style.gridColumn = "1/7";
+      reviewDiv.style.gridRow = "span 6";
       reviewDiv.style.padding = "0 1rem";
       reviewDiv.style.marginBottom = "0.3rem";
     } else {
